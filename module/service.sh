c20f861426cf790b0f387c51cf8e369e9b564375
crackxxxxx <axel_1995@inbox.ru>
2024-12-03 04:30:27 +0300

    v1.41

--- module/service.sh
+++ module/service.sh
@@ -1,18 +1,5 @@
 #!/system/bin/sh
 
-# Копируем скрипты в нужную директорию
-cp -f $MODPATH/system/bin/power_manager.sh /system/bin/power_manager.sh
-chmod 0755 /system/bin/power_manager.sh
-
-cp -f $MODPATH/system/bin/qos_manager.sh /system/bin/qos_manager.sh
-chmod 0755 /system/bin/qos_manager.sh
-
-cp -f $MODPATH/system/bin/qos_priority.sh /system/bin/qos_priority.sh
-chmod 0755 /system/bin/qos_priority.sh
-
-cp -f $MODPATH/system/bin/monitor_device_state.sh /system/bin/monitor_device_state.sh
-chmod 0755 /system/bin/monitor_device_state.sh
-
-# Запускаем скрипты
-/system/bin/monitor_device_state.sh &
-/system/bin/qos_priority.sh &
+if [ -f /data/adb/modules/optimization.lte/autostart ]; then
+        su -c "qos_priority start"
+fi
